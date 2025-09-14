208b11989c0a5dac8805601328391e4ec5972861
Arun Dhanapalan <104354229+ArunDhanapalan@users.noreply.github.com>
2025-09-14 22:36:44 +0530

    Update axiosConfig.js
    Added backend base URL

--- server/utils/axiosConfig.js
+++ server/utils/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000',
+  baseURL: 'https://civicpulse-316w.onrender.com' || 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json',
   },
